e6e5f343bd95b3af0c709e8bc284c0b9f62328dd
BHONESH CHAWLA <rajachawla778@gmail.com>
2018-04-02 23:27:52 +0530

    Add files via upload

---
+++ implementation_of_pwd.c
@@ -0,0 +1,33 @@
+/*
+***********************************************************
+***********************************************************
+
+NAME-BHONESH CHAWLA
+REGNO-20164017
+BATCH-CS-1
+CONTACT-+918619127663
+EMAIL-rajachawla778@gmail.com
+***********************************************************
+***********************************************************
+*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include<errno.h>
+#include<sys/wait.h>
+#include <unistd.h>
+#define pf printf
+#define sf scanf
+
+int main(){
+   
+	 char cwd[1000000];
+    getcwd(cwd, sizeof(cwd));
+    printf("Current working dir: %s\n", cwd);
+
+		
+
+
+
+}
+
